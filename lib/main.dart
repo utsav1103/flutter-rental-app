996f0b42eaca3c0434394cb19c5ac4b43251980f
utsav-baghela <utsavu858@gmail.com>
2023-03-28 18:14:00 +0530

    styling a Container

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,18 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx){
             return Card(child: Row(children: <Widget>[
-              Container(child: Text(tx.amount.toString(),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10,
+                horizontal: 15,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black,
+                  width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+              
+              child: Text(tx.amount.toString(),
               ),
               ),
               Column(children: <Widget>[
