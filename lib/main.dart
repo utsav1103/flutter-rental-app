f331a7cefee0f59e7d7ae8e1d563cf244bcafe22
utsav-baghela <utsavu858@gmail.com>
2023-03-30 19:28:17 +0530

    adding text input widgets

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -51,48 +51,77 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: transactions.map((tx){
-            return Card(child: Row(children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10,
-                horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Color.fromARGB(255, 249, 167, 241),
-                  width: 2,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-              
-              child: Text(
-                '\$${tx.amount}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.deepPurple,
-              )
-              ),
-              ), 
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
-                Text(tx.title,
-                style: TextStyle(fontSize: 16,
-                fontWeight: FontWeight.bold,
-                ),
-                ),
-                Text(
-                  DateFormat('yyyy/MM/dd').format(tx.date),
-                style: TextStyle(color:Colors.grey),),
-              ],
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  ElevatedButton(
+                    // ignore: prefer_const_constructors
+                    child: Text('Add Transaction',
+                    style: TextStyle(color: Color.fromARGB(255, 242, 229, 244)),
+                    ),
+                    
+                    onPressed: (){},
+                  ),
+                ],
               ),
-            ],
             ),
-            );
-  }).toList(),
-  ),
-        ], 
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Color.fromARGB(255, 249, 167, 241),
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text('\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.deepPurple,
+                          )),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('yyyy/MM/dd').format(tx.date),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
