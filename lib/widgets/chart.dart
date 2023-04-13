67b4d77122a4594f19d671f152fb0951d550b42c
utsav-baghela <utsavu858@gmail.com>
2023-04-13 20:30:42 +0530

    Creating Bars for our Char

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -3,34 +3,38 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
-
   final List<Transaction> recenttTransactions;
   Chart(this.recenttTransactions);
 
-  List<Map<String, Object>> get groupedTransactionValues{
+  List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: index),);
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
       var totalSum = 0.00;
-      for(var i = 0; i < recenttTransactions.length; i++){
-        if(recenttTransactions[i].date.day == weekDay.day && recenttTransactions[i].date.month == weekDay.month && recenttTransactions[i].date.year == weekDay.year){
-            totalSum += recenttTransactions[i].amount;
+      for (var i = 0; i < recenttTransactions.length; i++) {
+        if (recenttTransactions[i].date.day == weekDay.day &&
+            recenttTransactions[i].date.month == weekDay.month &&
+            recenttTransactions[i].date.year == weekDay.year) {
+          totalSum += recenttTransactions[i].amount;
         }
       }
 
-      return {'day': DateFormat.E().format(weekDay), 
-      'amount': totalSum
-      };
+      return {'day': DateFormat.E().format(weekDay).substring(0, 1),
+       'amount': totalSum};
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    return Card(elevation: 6,
-     margin: EdgeInsets.all(20),
-    child: Row(children: <Widget>[
-
-    ]),
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
+      ),
     );
   }
 }
