47784288ba2e3a448795313b132649c00262a01f
utsav-baghela <utsavu858@gmail.com>
2023-04-28 20:23:05 +0530

    finishing landscape mode

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,55 +11,62 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return transactions.isEmpty ? Column(children: <Widget> [
-        Text('NO transactions added yet!',
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          height: 200,
-          child: Image.asset(
-            'assets/images/waiting.jpg', fit: BoxFit.cover,)
-            ),
-      ],
-      )
-       : ListView.builder(
-        itemBuilder: (ctx, index){
-          return Card(
-            elevation: 5,
-            margin: EdgeInsets.symmetric(
-              vertical: 8,
-              horizontal: 5,
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                  padding: EdgeInsets.all(6),
-                  child: FittedBox(
-                    child: Text('\$${transactions[index].amount}'),
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints){
+          return Column(
+            children: <Widget>[
+              Text(
+                'NO transactions added yet!',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.jpg',
+                    fit: BoxFit.cover,
+                  )),
+            ],
+          );
+        }) 
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('\$${transactions[index].amount}'),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    style: TextButton.styleFrom(
+                      foregroundColor: Color.fromARGB(255, 141, 19, 10),
+                    ),
+                    onPressed: () => deleteTx(transactions[index].id),
                   ),
                 ),
-              ),
-              title: Text(
-                transactions[index].title,
-                style: TextStyle(color: Colors.black,
-                fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                DateFormat.yMMMd().format(transactions[index].date),
-              ),
-              trailing: IconButton(icon: Icon(Icons.delete),
-              style: TextButton.styleFrom(foregroundColor: Color.fromARGB(255, 141, 19, 10),
-              ),
-              onPressed: () => deleteTx(transactions[index].id),
-              ),
-            ),
+              );
+            },
+            itemCount: transactions.length,
           );
-        },
-        itemCount: transactions.length,
-        
-    );
   }
 }
