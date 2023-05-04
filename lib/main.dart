7a79c07628c7315597003b39b424e525f3c200b6
utsav-baghela <utsavu858@gmail.com>
2023-05-04 20:24:45 +0530

    using the safe area

--- lib/main.dart
+++ lib/main.dart
@@ -142,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
             0.7,
         child: TransactionList(_userTransactions, _deleteTransaction),
         );
-    final pageBody = SingleChildScrollView(
+    final pageBody = SafeArea(child : SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -183,8 +183,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   : txListWidget
           ],
         ),
-      );
-
+      ),
+    );
     return Platform.isIOS ? CupertinoPageScaffold(child: pageBody, navigationBar: appBar,) 
     : Scaffold(
       appBar: appBar,
